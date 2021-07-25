2e2c5cf78a61e70cbb7b66bb3261dd519de8e30d
MuhamadDoni <muhamaddoni1206@gmail.com>
2021-07-25 23:17:03 +0700

    UAS_Doni

---
+++ app/src/main/java/id/ac/uas_muhamaddoni/ui/AccountActivity.java
@@ -0,0 +1,33 @@
+package id.ac.uas_muhamaddoni.ui;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import id.ac.uas_muhamaddoni.R;
+
+public class AccountActivity extends AppCompatActivity {
+private Button btnmenu;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signin);
+        btnmenu = findViewById(R.id.btn_home);
+        btnmenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openLoginMain();
+            }
+        });
+    }
+    public void openLoginMain(){
+        Intent intent = new Intent(this, MainActivity2.class);
+        startActivity(intent);
+    }
+}
+
+
---
+++ app/src/main/java/id/ac/uas_muhamaddoni/ui/LoginActivity.java
@@ -0,0 +1,16 @@
+package id.ac.uas_muhamaddoni.ui;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import id.ac.uas_muhamaddoni.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
